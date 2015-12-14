07cc70c983750b98264340ce7a5affc93c96c1a2
mmpyhmm <choipui@gmail.com>
2015-12-14 17:31:21 -0600

    finish push selected meal to array feature with ng-change

--- client/scripts/controllers/createmenu.js
+++ client/scripts/controllers/createmenu.js
@@ -5,38 +5,56 @@ myApp.controller('CreateMenuController', ["$scope", "$http", "DataService", func
     $scope.dataService = DataService;
     $scope.allMeals = [];
     $scope.mealByCategory = {};
+    $scope.allergens = [];
     $scope.categories = $scope.dataService.getCategories();
+    $scope.currentMealArray = [];
 
     //pull in categories
     if ($scope.categories == undefined) {
         $scope.dataService.retrieveCategories().then(function(){
             $scope.categories = $scope.dataService.getCategories();
+            $scope.getMeals();
         });
     }else{
         $scope.categories = $scope.dataService.getCategories();
+        $scope.getMeals();
     }
 
-    // get meals for each category
-    $http.get('/createmenu').then(function(response){
-        //console.log(response.data);
-        $scope.allMeals = response.data;
+    // get meals for each category (excluding allergen information)
+    $scope.getMeals = function(){
+        $http.get('/createmenu/meals').then(function(response){
+            $scope.allMeals = response.data;
 
-        for(var i = 0; i < $scope.categories.length; i++){
-            $scope.categories[i].mealInfo = [];
-            for(var j = 0; j < $scope.allMeals.length; j++){
+            for(var i = 0; i < $scope.categories.length; i++){
+                $scope.categories[i].mealInfo = [];
 
-                if($scope.categories[i].category_name == $scope.allMeals[j].category_name){
+                for(var j = 0; j < $scope.allMeals.length; j++){
 
-                    //console.log($scope.categories[i].category_name);
-                    //$scope.mealByCategory.push($scope.allMeals[j]);
-
-                    //$scope.mealByCategory[$scope.categories[i].category_name].push($scope.allMeals[j]);
-                    $scope.categories[i].mealInfo.push($scope.allMeals[j]);
+                    // Push meal into object category based on category
+                    if($scope.categories[i].category_name == $scope.allMeals[j].category_name){
+                        $scope.categories[i].mealInfo.push($scope.allMeals[j]);
+                    }
                 }
             }
-        }
 
-        console.log($scope.categories);
+            // get allergen information for each meal
+            //for(i = 0; i < $scope.categories.length; i++){
+            //    for(j = 0; j < $scope.categories[i].mealInfo.length; j++) {
+            //        $http.get('/createmenu/allergens', {params: {mealId: $scope.categories[i].mealInfo[j].meal_id}}).then(function(response){
+            //            for(k = 0; k < response.data.length; k++){
+            //                $scope.allergens.push(response.data[k]);
+            //            }
+            //        });
+            //    }
+            //}
 
-    });
+        });
+    };
+
+    $scope.saveMenu = function(currentMeal){
+      console.log(currentMeal);
+        $scope.currentMealArray.push(currentMeal);
+        console.log($scope.currentMealArray);
+    };
+
 }]);
--- server/routes/createmenu.js
+++ server/routes/createmenu.js
@@ -7,11 +7,11 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/op
 
 
 // GET call to populate drop-down menus by categories
-router.get('/', function(req,res){
+router.get('/meals', function(req,res){
     var results = [];
 
     pg.connect(connectionString, function (err, client) {
-        var query = client.query("SELECT meals.entree, meals.side_1, meals.side_2, categories.category_name\
+        var query = client.query("SELECT meals.meal_id, meals.entree, meals.side_1, meals.side_2, categories.category_id, categories.category_name\
         FROM meals\
         JOIN meal_category ON meals.meal_id = meal_category.meal_id\
         JOIN categories ON categories.category_id = meal_category.category_id\
@@ -37,6 +37,36 @@ router.get('/', function(req,res){
     });
 });
 
+// GET call to query allergen information
+router.get('/allergens', function(req,res){
+    console.log(req.query);
+    var results = [];
 
+    pg.connect(connectionString, function (err, client) {
+        var query = client.query("SELECT meals.meal_id, allergens.allergen_name, allergenspecific.specific_name\
+        FROM meals\
+        JOIN meal_allergen_allergenspecific ON meal_allergen_allergenspecific.meal_id = meals.meal_id\
+        JOIN allergens ON meal_allergen_allergenspecific.allergen_id = allergens.allergen_id\
+        JOIN allergenspecific ON meal_allergen_allergenspecific.allergenspecific_id = allergenspecific.specific_id\
+        WHERE meals.meal_id = $1", [req.query.mealId]);
+
+
+        // Stream results back one row at a time, push into results array
+        query.on('row', function (row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function () {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if (err) {
+            console.log(err);
+        }
+    });
+});
 
 module.exports = router;
