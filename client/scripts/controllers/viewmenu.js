92a6255c7932659cd3268b3e1ddcf30e368ad34d
mmpyhmm <choipui@gmail.com>
2015-12-15 15:59:10 -0600

    complete show saved meals in dropdown

--- client/scripts/controllers/viewmenu.js
+++ client/scripts/controllers/viewmenu.js
@@ -1,3 +1,112 @@
-myApp.controller('ViewMenuController', ["$scope", "DataService", function($scope, DataService){
+myApp.controller('ViewMenuController', ["$scope", "$http", "DataService", function($scope, $http, DataService){
     console.log("view menu Controller Online");
+
+    // Declare Variables
+    $scope.dataService = DataService;
+    $scope.categories = $scope.dataService.getCategories();
+    $scope.menuByWeek = $scope.dataService.getMenu();
+    $scope.showCategoryHeadings = false;
+    $scope.menu = {};
+    $scope.allMeals = [];
+    $scope.mealsInMenuArray = [];
+    $scope.defaultMeal = [];
+
+    // Pull in categories
+    if($scope.categories == undefined){
+        $scope.dataService.retrieveCategories().then(function(){
+            $scope.categories = $scope.dataService.getCategories();
+            $scope.getMeals();
+        });
+    } else {
+        $scope.categories = $scope.dataService.getCategories();
+        $scope.getMeals();
+    }
+
+    // GET meals for each category
+    $scope.getMeals = function(){
+        $http.get('/createmenu/meals').then(function(response){
+
+            $scope.allMeals = response.data;
+
+            for(var i = 0; i < $scope.categories.length; i++){
+                $scope.categories[i].mealInfo = [];
+
+                for(var j = 0; j < $scope.allMeals.length; j++){
+
+                    // Push meal into object category based on category
+                    if($scope.categories[i].category_name == $scope.allMeals[j].category_name){
+                        $scope.categories[i].mealInfo.push($scope.allMeals[j]);
+                    }
+                }
+            }
+
+            // Build menuPreBuild Object for storing meals for each category
+            //for(var i = 0; i<$scope.categories.length; i++){
+            //    $scope.menuPreBuild[$scope.categories[i].category_name] = [];
+            //}
+
+        });
+    };
+
+    $scope.getMenu = function(menu) {
+
+        // Get Menu for Specific Week
+        if ($scope.menuByWeek == undefined){
+            $scope.dataService.retrieveMenuByWeek(menu.startDate, menu.endDate).then(function(){
+                $scope.menuByWeek = $scope.dataService.getMenu();
+                console.log($scope.menuByWeek);
+                $scope.setDefault();
+            });
+        }else {
+            $scope.menuByWeek = $scope.dataService.getMenu();
+            console.log($scope.menuByWeek);
+            $scope.setDefault();
+        }
+
+        $scope.showCategoryHeadings = true;
+
+        //$http.get('/getmenu', {params: {startDate: menu.startDate, endDate: menu.endDate}}).then(function (response) {
+        //    $scope.mealsInMenuArray = response.data;
+        //    $scope.showCategoryHeadings = true;
+
+            //for(var i = 0; i < $scope.categories.length; i++){
+            //    $scope.categories[i].mealInfo = [];
+            //
+            //    for(var j = 0; j < $scope.mealsInMenuArray.length; j++){
+            //
+            //        // Push meal into object category based on category
+            //        if($scope.categories[i].category_name == $scope.mealsInMenuArray[j].category_name){
+            //            $scope.categories[i].mealInfo.push($scope.mealsInMenuArray[j]);
+            //        }
+            //    }
+            //}
+
+
+
+
+            //$scope.defaultMeal[0] = $scope.categories[0].mealInfo[0];
+
+        //});
+    };
+
+    $scope.setDefault = function(){
+
+        for(var i = 0; i < $scope.categories.length; i++){
+            $scope.categories[i].defaultMeal = [];
+            for(var j = 0; j < $scope.categories[i].mealInfo.length; j++){
+                for(var k = 0; k < $scope.menuByWeek.length; k++){
+
+                    if($scope.menuByWeek[k].meal_id == $scope.categories[i].mealInfo[j].meal_id &&
+                        $scope.menuByWeek[k].category_id == $scope.categories[i].mealInfo[j].category_id){
+                        console.log("JACKPOT!");
+                            $scope.categories[i].defaultMeal.push($scope.categories[i].mealInfo[j]);
+                    }
+                }
+            }
+        }
+
+        //$scope.defaultMeal[0] = $scope.categories[1].mealInfo[0];
+    }
+
+
 }]);
--- server/routes/getmenu.js
+++ server/routes/getmenu.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
@@ -10,14 +9,11 @@ router.get('/', function(req,res){
     var results = [];
 
     pg.connect(connectionString, function (err, client) {
-        var query = client.query("SELECT menus.start_date, menus.end_date, meals.entree, meals.side_1, meals.side_2, categories.category_name, allergens.allergen_name, allergenspecific.specific_name\
+        var query = client.query("SELECT meals.meal_id, meals.entree, meals.side_1, meals.side_2, categories.category_id, categories.category_name\
         FROM menus\
         JOIN meal_menu ON meal_menu.menu_id = menus.menu_id\
         JOIN meals ON meals.meal_id = meal_menu.meal_id\
         JOIN categories ON categories.category_id = meal_menu.category_id\
-        JOIN meal_allergen_allergenspecific ON meal_allergen_allergenspecific.meal_id = meals.meal_id\
-        JOIN allergens ON meal_allergen_allergenspecific.allergen_id = allergens.allergen_id\
-        JOIN allergenspecific ON meal_allergen_allergenspecific.allergenspecific_id = allergenspecific.specific_id\
         WHERE menus.start_date >= $1 AND menus.start_date <= $2\
         ORDER BY menus.start_date ASC, categories.category_id ASC",
             [req.query.startDate, req.query.endDate]);
