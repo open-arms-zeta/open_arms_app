442cd6ca7decf412641fc884b8b38680cd6e9e7e
tehat <thomasehartle@gmail.com>
2015-12-17 12:29:02 -0600

    POST AND GET calls for create_meal

--- client/scripts/controllers/createmeal.js
+++ client/scripts/controllers/createmeal.js
@@ -7,6 +7,7 @@ myApp.controller('CreateMealController', ["$scope",  '$http', "DataService",  fu
         allergens: [],
         status: true
     };
+    $scope.mealID;
 
     //CATEGORIES PRESENTED FOR SELECTION
     $scope.mealCategories = function(){
@@ -32,26 +33,61 @@ myApp.controller('CreateMealController', ["$scope",  '$http', "DataService",  fu
     };
     $scope.mealAllergens();
 
-    //SEND NEW MEAL AND ALLERGEN SELECTION TO DB
-    $scope.submit = function(request){
-        console.log("submitr", request);
-        console.log('here is the meal', $scope.meal);
-        //$http({
-        //    method: 'POST',
-        //    url: '/postmeals'
-        //}).then (function(req){
-        //$scope.clearInput();
-        //
-        //});
 
-        console.log($scope.meal);
+    //SEND NEW MEAL AND ALLERGEN SELECTION TO DB
+    $scope.submit = function(){
+        console.log('here is the meal', $scope.meal);
+        $http({
+            method: 'POST',
+            url: '/postmeals/saveToMealsTable',
+            data: $scope.meal
+        }).then (function(response){
+            //console.log($scope.meal_id);
+            //console.log(response.data);
+
+            //$scope.mealId = response.data[0].meal_id;
+            //$scope.clearInput();
+            $scope.newMealID();
+
+        });
+
+        //console.log($scope.meal);
 
     };
+
+    //NEW MEAL_ID NEWLY CREATED MEAL
+    $scope.newMealID = function(){
+        $http({
+            method: 'GET',
+            url: "/postmeals/getNewMealID"
+        }).then(function(response){
+            console.log("This is the mealID coming back", response.data[0]);
+            //$scope.meal = response.data;
+            $scope.mealId = response.data[0].meal_id;
+
+            // Call function to post meal_id, category_id into meal_category table
+            $scope.postToMealCategory();
+
+            // Call function to post info into meal_allergens_allergenspecific table
+
+
+        });
+    };
+
+    $scope.postToMealCategory = function(){
+        $http({
+            method: 'POST',
+            url: "/postmeals/postToMealCategory",
+            data: {mealId: $scope.mealId, mealObject: $scope.meal}
+        }).then(function(){
+
+        });
+    };
+
 
     $scope.clearInput = function() {
         $scope.meal = {};
         $scope.createMeal.$setUntouched();
-
     };
 
     $scope.findAllergen = function(allergen){
@@ -62,7 +98,6 @@ myApp.controller('CreateMealController', ["$scope",  '$http', "DataService",  fu
     $scope.associate = function(allergen, specificAllergen){
         var index = _.findLastIndex($scope.meal.allergens, {allergen_name: allergen});
         $scope.meal.allergens[index].specific = specificAllergen;
-        //console.log($scope.meal);
     };
 
 
--- server/routes/postmeals.js
+++ server/routes/postmeals.js
@@ -7,21 +7,73 @@ var pg = require('pg');
 
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/open_arms_db';
 
-router.post('/', function(req, res){
+router.post('/saveToMealsTable', function(req, res){
+
+    //console.log(req.body);
+
     var postMeals = {
         "entree": req.body.entree,
         "side_1": req.body.side_1,
         "side_2": req.body.side_2,
         "status": req.body.status
-        //"allergen_name": req.body.allergen_name,
-        //"is_specific": req.body.is_specific,
-        //"specific_name": req.body.specific_name
     };
-    console.log("get request", addMailing);
+    console.log("post request", postMeals);
 
     pg.connect(connectionString, function(err, client){
         client.query("INSERT INTO meals (entree, side_1, side_2, status) VALUES ($1, $2, $3, $4)",
             [postMeals.entree, postMeals.side_1, postMeals.side_2, postMeals.status],
+            function(err, result) {
+                if (err) {
+                    console.log("error with inserting data", err);
+                    res.send(false);
+                }
+
+                res.send(result);
+            });
+    });
+
+});
+
+
+router.get('/getNewMealID', function(req,res){
+    var results = [];
+
+    pg.connect(connectionString, function (err, client) {
+        var query = client.query("SELECT meal_id\
+        FROM meals\
+        ORDER BY meal_id DESC");
+
+        // Stream results back one row at a time, push into results array
+        query.on('row', function (row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function () {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+
+
+
+router.post('/', function(req, res){
+    var mealAllergenAllergenspecific = {
+        "meal_id": req.body.meal_id,
+        "allergen_id": req.body.allergen_id
+    };
+    console.log("post request", mealAllergenAllergenspecific);
+
+    pg.connect(connectionString, function(err, client){
+        client.query("INSERT INTO meal_allergen (meal_id, allergen_id) VALUES ($1, $2)",
+            [mealAllergenAllergenspecific.meal_id, mealAllergenAllergenspecific.allergen_id, mealAllergenAllergenspecific.allergenspecific_id],
             function(err, result) {
                 if (err) {
                     console.log("error with inserting data", err);
@@ -33,5 +85,40 @@ router.post('/', function(req, res){
     });
 
 });
+
+router.post('/postToMealCategory', function(req, res){
+
+    console.log(req.body);
+
+    var mealCategory = {
+        "meal_id": req.body.mealId
+    };
+
+    pg.connect(connectionString, function (err, client){
+
+        for(var i = 0; i < req.body.mealObject.categories.length; i++) {
+
+            mealCategory.category_id = req.body.mealObject.categories[i].category_id;
+            console.log(mealCategory);
+
+            var q = "INSERT INTO meal_category (meal_id, category_id) VALUES ($1, $2)";
+            console.log("query: ", q);
+            console.log(mealCategory.meal_id, mealCategory.category_id);
+            var result = client.query(q, [mealCategory.meal_id, mealCategory.category_id]);
+        }
+
+        if(err) console.log(err);
+
+        result.on('end', function () {
+            client.end();
+        });
+
+    });
+
+    res.send("save success");
+
+
+});
+
 
 module.exports = router;
