392b35224365045fa8e4b46b4c199d86b3ec2506
Aron <athomas5@macalester.edu>
2016-01-05 11:22:34 -0600

    unique email check

--- client/scripts/controllers/addclient.js
+++ client/scripts/controllers/addclient.js
@@ -7,8 +7,16 @@ myApp.controller('AddClientController', ["$scope", "DataService", "$http", "$uib
     console.log("Add client Controller Online");
     $scope.dataService = DataService;
     $scope.categories = undefined;
+    $scope.clients = undefined;
     $scope.newClient = {};
 
+
+    if($scope.clients == undefined){
+        $http.get('/register/all').then(function(response){
+            $scope.clients = response.data;
+            console.log('clients in db', $scope.clients);
+        })
+    }
 
     if ($scope.categories == undefined) {
         $scope.dataService.retrieveCategories().then(function(){
@@ -57,7 +65,45 @@ myApp.controller('AddClientController', ["$scope", "DataService", "$http", "$uib
         modalInstance.result.then(function(){
             console.log('modal closed')
         });
+    };
+
+    $scope.clientExists = function(clientEmail){
+        console.log( _.findWhere($scope.clients, {email: clientEmail}));
+        return $scope.$apply(function(){
+            if(_.findWhere($scope.clients, {email: clientEmail})){
+                $scope.addClientForm.inputEmail.$setValidity('unique', false);
+                //addClientForm.inputEmail.$duplicate = true;
+                return true
+            }else{
+                $scope.addClientForm.inputEmail.$setValidity('unique', true);
+                //addClientForm.inputEmail.$duplicate = false;
+                return false
+            }
+        });
+        //return _.findWhere($scope.clients, {email: clientEmail});
     }
+}]);
+
+myApp.directive('isUnique', ['$http', function($http){
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            element.on('keyup', function(){
+                var entry = element[0].value;
+                return $http.get('/register/all').then(function(response){
+                    var clients = response.data;
+                    if(_.findWhere(clients, {email: entry}) && entry.length >0){
+                        console.log('true!');
+                        ctrl.$setValidity('isunique', false);
+                    }else{
+                        console.log('false!');
+                        ctrl.$setValidity('isunique', true);
+                    }
+                });
+            });
+        }
+    };
 }]);
 
 myApp.controller('ImportClientController', ["$scope", "$uibModalInstance", "$http", "DataService", function($scope, $uibModalInstance, $http, DataService){
