81dc9913b5029339f6f29cbf4b8c4fc944892208
mmpyhmm <choipui@gmail.com>
2016-01-20 16:34:52 -0600

    change modal to angular material dialogs

--- client/scripts/controllers/clientwelcome.js
+++ client/scripts/controllers/clientwelcome.js
@@ -1,4 +1,5 @@
-myApp.controller('ClientWelcomeController', ["$scope", "DataService", "$http", function($scope, DataService, $http){
+myApp.controller('ClientWelcomeController', ["$scope", "DataService", "$http", "$mdDialog", "$mdMedia",
+    function($scope, DataService, $http, $mdDialog, $mdMedia){
     console.log("Client Welcome Controller Online");
 
     $scope.dataService = DataService;
@@ -223,6 +224,7 @@ myApp.controller('ClientWelcomeController', ["$scope", "DataService", "$http", f
                console.log("This is orderedMealsArray", $scope.orderedMealsArray);
                $scope.mealsChosen = true;
                $scope.customized = false;
+               $scope.$apply();
            }
 
         });
@@ -237,7 +239,7 @@ myApp.controller('ClientWelcomeController', ["$scope", "DataService", "$http", f
 
     //  This function runs when client confirms default meal selection
     $scope.postDefaultMeal = function(){
-
+        //$scope.orderToPost = [];
         $scope.modalShown = !$scope.modalShown;
 
         for(var i = 0; i < $scope.menu.length; i++){
@@ -297,7 +299,7 @@ myApp.controller('ClientWelcomeController', ["$scope", "DataService", "$http", f
         $scope.badgeNumber = $scope.addedMealArray.length;
     };
 
-    // POST custom meals to Clients Order table
+    //// POST custom meals to Clients Order table
     $scope.postCustomMeal = function(){
 
         $scope.uniqueMealArray = _.uniq($scope.addedMealArray);
@@ -340,5 +342,260 @@ myApp.controller('ClientWelcomeController', ["$scope", "DataService", "$http", f
         $scope.modalShown3 = false;
     };
 
+    //ANGULAR MATERIAL DIALOG FOR INSTRUCTIONS!!!!
+
+    $scope.status = '  ';
+    $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+    $scope.showAdvanced = function(ev) {
+
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $mdDialog.show({
+            controller: 'DialogController',
+            templateUrl: '/assets/views/templates/clientinstruction.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: useFullScreen
+        });
+
+    };
+
+    $scope.showAdvancedMeal = function(ev) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $mdDialog.show({
+            controller: 'CustomizeController',
+            templateUrl: '/assets/views/templates/clientmealconfirmation.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: useFullScreen,
+            locals:{
+                menu: $scope.menu,
+                addedMealArray: $scope.addedMealArray,
+                activeWeek: $scope.activeWeek,
+                user: $scope.user
+            }
+        })
+            .then(function(){
+                console.log('closed')
+                $scope.checkHasOrdered();
+            });
+
+    };
+
+    $scope.showAdvancedCustomize = function(ev) {
+        console.log('added meals', $scope.addedMealArray);
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $mdDialog.show({
+            controller: 'CustomizeController',
+            templateUrl: '/assets/views/templates/clientmealcustomizeconfirmation.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: useFullScreen,
+            locals:{
+                menu: $scope.menu,
+                addedMealArray: $scope.addedMealArray,
+                activeWeek: $scope.activeWeek,
+                user: $scope.user
+            }
+        })
+            .then(function(){
+                console.log('doing stuff')
+                $scope.checkHasOrdered();
+            });
+
+    };
+
+
+
 }]);
+
+myApp.controller('CustomizeController', ['$scope', '$http', '$mdDialog', 'menu', 'addedMealArray', 'activeWeek', 'user',
+    function($scope, $http, $mdDialog, menu, addedMealArray, activeWeek, user){
+
+        console.log('controller menu', menu);
+        console.log('controller meals added', addedMealArray);
+        $scope.activeWeek = activeWeek;
+        $scope.addedMealArray = addedMealArray;
+        $scope.menu = menu;
+        $scope.user = user;
+        $scope.orderToPost = [];
+
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+
+        // Check if client has already ordered!
+        //$scope.checkHasOrdered = function(){
+        //
+        //    return $http.get('/getclients/checkOrdered', {params: {clientId: $scope.user.id, menuId: $scope.menu[0].menu_id}}).then(function(response){
+        //
+        //        if (response.data[0]) {
+        //            console.log("This person ordered...", response.data);
+        //            $scope.orderedMealsArray = (response.data);
+        //            console.log("This is orderedMealsArray", $scope.orderedMealsArray);
+        //            $scope.mealsChosen = true;
+        //            $scope.customized = false;
+        //        }
+        //
+        //    });
+        //};
+
+        $scope.postDefaultMeal = function(){
+            //$scope.orderToPost = [];
+            $scope.modalShown = !$scope.modalShown;
+
+            for(var i = 0; i < $scope.menu.length; i++){
+                if($scope.user.category_id == $scope.menu[i].category_id){
+                    $scope.orderToPost.push({
+                        clientId: $scope.user.id,
+                        menuId: $scope.menu[0].menu_id,
+                        mealId: $scope.menu[i].meal_id,
+                        categoryId: $scope.menu[i].category_id,
+                        count: 1
+                    });
+                }
+            }
+
+            return $http.post('/postclientorders/saveClientOrders', $scope.orderToPost).then(function(){
+                $mdDialog.hide();
+                //$scope.checkHasOrdered();
+            });
+
+        };
+        // POST custom meals to Clients Order table
+        $scope.postCustomMeal = function(){
+            $scope.uniqueMealArray = _.uniq($scope.addedMealArray);
+
+            // Create orderToPost array with uniqueMealArray information
+            for(var i = 0; i < $scope.uniqueMealArray.length; i++) {
+                $scope.orderToPost.push({
+                    clientId: $scope.user.id,
+                    menuId: $scope.menu[0].menu_id,
+                    mealId: $scope.uniqueMealArray[i].meal_id,
+                    categoryId: $scope.uniqueMealArray[i].category_id,
+                    count: 0
+                });
+            }
+
+            // Compare uniqueMealArray and addedMealArray for duplicates
+            for(var i = 0; i < $scope.uniqueMealArray.length; i++) {
+                for (var j = 0; j < $scope.addedMealArray.length; j++){
+
+                    if($scope.uniqueMealArray[i].meal_id == $scope.addedMealArray[j].meal_id &&
+                        $scope.uniqueMealArray[i].category_id == $scope.addedMealArray[j].category_id){
+
+                        // Add to count in orderToPost for number of each meal selected
+                        $scope.orderToPost[i].count += 1;
+
+                    }
+                }
+
+            }
+
+            $http.post('/postclientorders/saveClientOrders', $scope.orderToPost).then(function(){
+                //$scope.checkHasOrdered();
+                $mdDialog.hide();
+            });
+
+            $scope.modalShown2 = !$scope.modalShown2;
+        };
+    }]);
+
+myApp.controller('DialogController', ['$scope', '$mdDialog', function($scope, $mdDialog){
+    $scope.instructionNumber = 0;
+
+    $scope.instructionImages = [
+        {
+            title: 'Welcome Screen',
+            url: "http://res.cloudinary.com/dhro0fkhc/image/upload/v1452359700/screen_welcome_highlight_qhrw5z.png"
+        },
+        {
+            title: 'Meal Selection',
+            url: "http://res.cloudinary.com/dhro0fkhc/image/upload/v1452359701/screen_chose_meals_1_highlight_s1iy0v.png"
+        },
+        {
+            title: 'Meal Selection (Continued)',
+            url: "http://res.cloudinary.com/dhro0fkhc/image/upload/v1452363827/screen_choose_meals_2_highlight_fl3wkj.png"
+        },
+        {
+            title: 'Confirmation Window',
+            url: "http://res.cloudinary.com/dhro0fkhc/image/upload/v1452359801/confirmation_window.png"
+        },
+        {
+            title: 'Order Complete',
+            url: "http://res.cloudinary.com/dhro0fkhc/image/upload/v1452359701/screen_confirm_uxx2s0.png"
+        },
+        {
+            title: 'Inactive User',
+            url: "http://res.cloudinary.com/dhro0fkhc/image/upload/v1452359700/screen_inactive_ggwf3r.png"
+        }
+    ];
+
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+    };
+
+    $scope.nextInstruction = function(){
+        if($scope.instructionNumber == Object.keys($scope.instructionImages).length - 1){
+            $scope.instructionNumber = 0;
+        }else{
+            $scope.instructionNumber++;
+        }
+        //$scope.setImage()
+
+    };
+
+    $scope.prevInstruction = function(){
+        if($scope.instructionNumber ==0){
+            $scope.instructionNumber = Object.keys($scope.instructionImages).length - 1;
+        }else{
+            $scope.instructionNumber--;
+        }
+        //$scope.setImage()
+    };
+}])
+//function DialogController($scope, $mdDialog) {
+//    $scope.instructionNumber = 0;
+//
+//    $scope.hide = function() {
+//        $mdDialog.hide();
+//    };
+//
+//    $scope.cancel = function() {
+//        $mdDialog.cancel();
+//    };
+//
+//    $scope.answer = function(answer) {
+//        $mdDialog.hide(answer);
+//    };
+//
+//    $scope.nextInstruction = function(){
+//        if($scope.instructionNumber == Object.keys($scope.instructionImages).length - 1){
+//            $scope.instructionNumber = 0;
+//        }else{
+//            $scope.instructionNumber++;
+//        }
+//        //$scope.setImage()
+//
+//    };
+//
+//    $scope.prevInstruction = function(){
+//        if($scope.instructionNumber ==0){
+//            $scope.instructionNumber = Object.keys($scope.instructionImages).length - 1;
+//        }else{
+//            $scope.instructionNumber--;
+//        }
+//        //$scope.setImage()
+//    };
+//}
 
